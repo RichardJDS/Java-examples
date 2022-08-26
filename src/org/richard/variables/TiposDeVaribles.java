40d8f4b0ff9bf32376daeb3b77ebd61f990fde30
Richard Jhony Duran Sullcamani <duransullcamanirichard@gmail.com>
2022-08-26 18:55:43 -0400

    Tarea 1

---
+++ src/org/richard/variables/TiposDeVaribles.java
@@ -0,0 +1,33 @@
+package org.richard.variables;
+
+public class TiposDeVaribles {
+    public static void main(String[] args) {
+//        TIPO NUMERICO
+
+        int entero = 52;
+        System.out.println("entero = " + entero);
+        byte numero1 = 4;
+        System.out.println("numero1 = " + numero1);
+        long numero2 = 23;
+        System.out.println("numero2 = " + numero2);
+        float decimal = 12.8F;
+        System.out.println("decimal = " + decimal);
+        double decimal2 = 456.89D;
+        System.out.println("decimal2 = " + decimal2);
+
+//        BOOLEAN
+        boolean state = true;
+        System.out.println("state = " + state);
+        boolean state2 = false;
+        System.out.println("state2 = " + state2);
+
+//        Cadenas
+        String nombre = "Richard";
+        System.out.println("nombre = " + nombre);
+        char character = 'R';
+        System.out.println("character = " + character);
+
+
+        
+    }
+}
